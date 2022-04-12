37aa2d320aab3ce7633e0236700e51d5a4d5a090
Bart Jaskulski <bjaskulski@protonmail.com>
2022-04-12 21:10:14 +0200

    added integration with Fluid Checkout

---
+++ includes/Extensions/FluidCheckout.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CleanWeb\Extensions;
+
+use FluidCheckout_Steps;
+
+class FluidCheckout implements \CleanWeb\Component {
+
+	public function initialize(): void {
+		add_filter( 'fc_override_template_with_theme_file', [ $this, 'override_template' ], 10, 4 );
+		add_action(
+			'init',
+			function () {
+				remove_action( 'woocommerce_before_checkout_form_cart_notices', [ FluidCheckout_Steps::instance(), 'output_checkout_progress_bar' ], 4 );
+			}
+		);
+	}
+
+	public function override_template( bool $override, $template, $template_name, $template_path ): bool {
+		$override_templates = [
+//			'fc/checkout/form-contact.php',
+			'checkout/review-order.php',
+			'fc/checkout/review-order-section.php',
+		];
+
+		if ( in_array( $template_name, $override_templates, true ) ) {
+			return true;
+		}
+
+		return $override;
+	}
+}
