fa875e47b05f0f1b39ae202a1b8370e6dba0aeda
Bart Jaskulski <bjaskulski@protonmail.com>
2022-05-21 15:56:55 +0200

    Remove unnecessary TS files

--- assets/js/admin.ts
+++
@@ -1,9 +0,0 @@
-import ActionAttachment from './admin/shop-meta'
-import elementReady from "element-ready";
-
-(async () => {
-	const el = await elementReady('#slider-id')
-	if (!el) return
-	new ActionAttachment(el.id).bindEvents()
-})()
-
--- assets/js/menu.ts
+++
@@ -1,8 +0,0 @@
-const menuContainer = document.querySelector<HTMLDetailsElement>('.js-menu-container')
-
-export default (e: MediaQueryListEvent|MediaQueryList): void => {
-	if (!menuContainer) return
-
-	if (e.matches) menuContainer.setAttribute('open','')
-	else menuContainer.removeAttribute('open')
-}
