133d1758ade781691264848b273550701a43e25f
Bart Jaskulski <bjaskulski@protonmail.com>
2022-05-25 01:23:17 +0200

    Use standard "loop" for templating

--- templates/entry.php
+++ templates/entry.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * @var WP_Post[] $posts
+ * @var array{posts: WP_Post[]} $args
  */
 
 ?>
 <div class="basis-[680px]">
-	<?php foreach ( $posts as $post ) : ?>
+	<?php while (have_posts()) { ?>
+		<?php the_post(); ?>
 		<article class="l-flow" data-flow-size="lg">
 			<h1><?php the_title(); ?></h1>
 			<div>
 				<?php the_content(); ?>
 			</div>
 		</article>
-	<?php endforeach; ?>
-	<?php get_template_part( 'templates/partial/pagination', args: [ 'posts' => $posts ] ); ?>
+	<?php } ?>
+	<?php get_template_part( 'templates/partial/pagination', args: [ 'posts' => $args['posts'] ] ); ?>
 </div>
